2f651dad028c7c7b59e0d22b5fb84d5be28c8b32
sdarwin <samuel.d.darwin@gmail.com>
2021-01-21 22:14:37 +0000

    add drone config [ci skip]

---
+++ .github/scripts/boost.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Copyright 2020 Rene Rivera, Sam Darwin
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
+
+set -ex
+export TRAVIS_BUILD_DIR=$(pwd)
+export TRAVIS_BRANCH=${TRAVIS_BRANCH:-$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')}
+export VCS_COMMIT_ID=$GITHUB_SHA
+export GIT_COMMIT=$GITHUB_SHA
+export REPO_NAME=$(basename $GITHUB_REPOSITORY)
+export PATH=~/.local/bin:/usr/local/bin:$PATH
+
+echo '==================================> INSTALL'
+
+git clone https://github.com/jeking3/boost-ci.git boost-ci
+cp -pr boost-ci/ci boost-ci/.codecov.yml .
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    unset -f cd
+fi
+
+export SELF=`basename $REPO_NAME`
+export BOOST_CI_TARGET_BRANCH="$TRAVIS_BRANCH"
+export BOOST_CI_SRC_FOLDER=$(pwd)
+
+. ./ci/common_install.sh
+
+echo '==================================> SCRIPT'
+
+cd $BOOST_ROOT/libs/$SELF
+ci/travis/build.sh
